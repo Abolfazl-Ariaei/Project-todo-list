5db7342e872d473bf417c520c8f17e70a89037fd
Your Name <you@example.com>
2023-08-22 16:49:29 +0330

    V1

--- app.js
+++ app.js
@@ -6,48 +6,5 @@ const btnedit = document.querySelectorAll('.but__edit');
 const btndelet = document.querySelectorAll('.but__delet');
 const heder = document.querySelector('.sections');
 
-btninput.addEventListener('click', (e) => {
-    e.preventDefault();
-    const todoText = text.value;
-    if (!todoText) {
-        alert('not value');
-    } else {
-        const taks_al = document.createElement('div');
-        taks_al.classList.add('todo');
 
-        const taks_contanet = document.createElement('input');
-        taks_contanet.classList.add('input__todo_text');
-
-        const acshondeldetedeit = document.createElement('div');
-        acshondeldetedeit.classList.add('delet__edit');
-
-        const edit = document.createElement('button');
-        edit.classList.add("edit");
-        const deletes = document.createElement('button');
-        deletes.classList.add("delet");
-        // appendchild
-        acshondeldetedeit.appendChild(edit)
-        acshondeldetedeit.appendChild(deletes)
-        taks_al.appendChild(taks_contanet);
-        heder.appendChild(taks_al);
-        taks_al.appendChild(acshondeldetedeit)
-
-        // text content
-        taks_contanet.value = todoText;
-        edit.textContent = 'edit';
-        deletes.textContent = 'delete';
-
-        deletes.addEventListener('click', (e) => {
-            taks_al.style.display = 'none';
-        });
-        edit.addEventListener('click', (e) => {
-            if (edit.innerText == 'edit') {
-                taks_contanet.focus();
-                edit.textContent = 'seve';
-            } else {
-                edit.textContent = 'edit';
-            };
-        });
-    }
-});
 
